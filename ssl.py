fb9fcc6764d62d35ea22aa59750a36f4294770da
pseudorandomID <qusdudwowo@gmail.com>
2017-03-26 20:34:55 +0900

    Create ssl.py

---
+++ ssl.py
@@ -0,0 +1,10 @@
+def _get_httpd_context_factory():
+    if not sys.flags.ignore_environment:
+        config_setting = os.environ.get(_https_verify_envvar)
+        if config_setting == '0':
+            _create_unverified_context
+    return _create_unvierfied_context #original : create_default_context
+ 
+ 
+def _https_verify_certificates(enable=False): #original : enable=True
+
